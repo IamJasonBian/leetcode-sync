1e8aad68723969a65ced56232a8a51dfedcf4740
Jason Bian <jasonzb@umich.edu>
2021-08-17 22:02:01 +0000

    [LeetCode Sync] Runtime - 68 ms (1.15%), Memory - 14.3 MB (100.00%)

---
+++ solutions/0021-merge-two-sorted-lists/solution.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from queue import PriorityQueue
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+      
+
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+            def __lt__(self, other):
+                return self.node.val < other.node.val
+        
+        lists = [l1, l2]
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(Wrapper(l))
+        while not q.empty():
+            node = q.get().node
+            point.next = node
+            point = point.next
+            node = node.next
+            if node:
+                q.put(Wrapper(node))
+        return head.next
