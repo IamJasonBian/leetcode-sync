688966933a854b370d8286c4e7e340432ddeb64f
Jason Bian <jasonzb@umich.edu>
2021-09-27 06:00:58 +0000

    [LeetCode Sync] Runtime - 430 ms (63.91%), Memory - 0.0B (100.00%)

---
+++ solutions/0197-rising-temperature/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select 
+    weather.id as 'Id'
+from 
+    weather 
+        join
+    weather w on datediff(weather.recordDate, w.recordDate) = 1
+        And weather.Temperature > w.Temperature
