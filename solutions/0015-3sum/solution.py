449e1dd7275a23dea7008076fd91bd675b390c14
Jason Bian <jasonzb@umich.edu>
2023-05-27 00:25:21 +0000

    [LeetCode Sync] Runtime - 1494 ms (9.28%), Memory - 20.5 MB (74.80%)

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -1,24 +1,28 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
         nums.sort()
-        N, result = len(nums), []
-        for i in range(N):
-            if i > 0 and nums[i] == nums[i-1]:
+        ans = []
+        
+        for idx in range(len(nums) - 2):
+            a = nums[idx]
+
+            if idx != 0 and a == nums[idx - 1]:
                 continue
-            target = nums[i]*-1
-            s,e = i+1, N-1
-            while s<e:
-                if nums[s]+nums[e] == target:
-                    result.append([nums[i], nums[s], nums[e]])
-                    s = s+1
-                    while s<e and nums[s] == nums[s-1]:
-                        s = s+1
-                elif nums[s] + nums[e] < target:
-                    s = s+1
+            l = idx + 1
+            r = len(nums) - 1
+            
+            while l < r:
+                b = nums[l]
+                c = nums[r]
+                threeSum = a + b + c
+                if threeSum < 0:
+                    l += 1
+                elif threeSum > 0:
+                    r -= 1
                 else:
-                    e = e-1
-        return result
+                    ans.append([a, b, c])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return ans
