fbc698b217680a59701ae897b93d6e9b0e1f4ae6
Jason Bian <jasonzb@umich.edu>
2023-05-27 00:01:36 +0000

    [LeetCode Sync] Runtime - 62 ms (33.92%), Memory - 17.8 MB (96.10%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,9 +1,11 @@
-from itertools import combinations
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = {}
-        for i, num in enumerate(nums):
-            if target - num in lookup:
-                return (lookup[target - num], i )
-            lookup[num] = i
+        
+        values = {}
+        
+        for idx, value in enumerate(nums):
+
+            if target - value in values:
+                return [values[target - value], idx]
+            else:
+                values[value] = idx
