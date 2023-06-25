40c16209730a471746ae01cb7365a0aec38b76d9
Jason Bian <jasonzb@umich.edu>
2023-06-25 21:50:54 +0000

    [LeetCode Sync] Runtime - 102 ms (7.59%), Memory - 20.5 MB (100.00%)

---
+++ solutions/0789-kth-largest-element-in-a-stream/solution.py
@@ -0,0 +1,27 @@
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+import heapq
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+
+        #Reflexive on self.nums object
+        heapify(self.nums) 
+
+        while len(self.nums) > self.k:
+            heappop(self.nums)
+        
+    def add(self, val: int) -> int:
+        if len(self.nums) < self.k:
+            heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapreplace(self.nums, val)
+        
+        return self.nums[0]
+
+
+
+    
