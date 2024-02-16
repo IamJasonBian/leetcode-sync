9a1f0a3e9271b7697e5774a5777ff2d7774117b3
Jason Bian <jasonzb@umich.edu>
2024-02-16 08:35:26 +0000

    [LeetCode Sync] Runtime - 779 ms (35.54%), Memory - 16.9 MB (100.00%)

--- solutions/0322-coin-change/solution.py
+++ solutions/0322-coin-change/solution.py
@@ -2,17 +2,14 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Initialize dp list with infinity for all amounts except  0
-        dp = [float('inf')] * (amount +  1)
-        dp[0] =  0  # Base case:  0 coins needed to make up amount  0
-        
-        # Iterate through each coin denomination
-        for coin in coins:
-            # Update dp values for all amounts greater than or equal to coin value
-            for x in range(coin, amount +  1):
-                # Take the minimum between the current dp value and the dp value for the amount minus the coin value plus one
-                dp[x] = min(dp[x], dp[x - coin] +  1)
-        
-        # Return -1 if no combination can sum to the amount, otherwise return the minimum number of coins
-        return -1 if dp[amount] == float('inf') else dp[amount]
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a-c])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
 
