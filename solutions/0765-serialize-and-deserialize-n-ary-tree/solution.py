a37c34332f14a06620ffa166370dc57f8021ffbd
Jason Bian <jasonzb@umich.edu>
2024-04-12 05:13:51 +0000

    [LeetCode Sync] Runtime - 55 ms (92.83%), Memory - 18.8 MB (100.00%)

---
+++ solutions/0765-serialize-and-deserialize-n-ary-tree/solution.py
@@ -0,0 +1,45 @@
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: Node
+        :rtype: str
+        """
+        result = []
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node == None:
+                result.append("null")
+            else:
+                queue.append(None)
+                result.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+
+        while result and result[-1] == "null":
+            result.pop()
+            
+        return ",".join(str(x) for x in result)
+
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: Node
+        """
+        parentNode = rootNode = None
+        queue = deque()
+
+        for item in data.split(","):
+            if item == "null":
+                parentNode = queue.popleft()
+            elif item:
+                childNode = Node(int(item), [])
+                queue.append(childNode);
+                if rootNode is None:
+                    rootNode = childNode
+                else:
+                    parentNode.children.append(childNode)
+
+        return rootNode
