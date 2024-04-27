518221ae02fc8c24dc975a94b7e5081719c3a2ab
Jason Bian <jasonzb@umich.edu>
2024-04-27 22:19:44 +0000

    [LeetCode Sync] Runtime - 1 ms (96.66%), Memory - 42.8 MB (99.96%)

---
+++ solutions/0107-binary-tree-level-order-traversal-ii/solution.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
+        
+        // Reverse the result to get the bottom-up order
+        Collections.reverse(result);
+        return result;
+    }
+}
+
