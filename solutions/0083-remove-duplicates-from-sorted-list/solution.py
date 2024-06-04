9ce9314c502f1630718da27a366cdaeb2881a8af
Jason Bian <jasonzb@umich.edu>
2024-06-04 23:33:35 +0000

    [LeetCode Sync] Runtime - 32 ms (2.11%), Memory - 16.5 MB (100.00%)

--- solutions/0083-remove-duplicates-from-sorted-list/solution.py
+++ solutions/0083-remove-duplicates-from-sorted-list/solution.py
@@ -1,51 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-       #for i in list node
-       #store into set, 
-       #de dup set
-       #rebuild linked list from set
-
-       #traverse linked list
-       #push into queue, check if queue exists
-       #rebuild from queue
-
-       #traverse linked list
-       #push into queue, check if queue exists (dict)
-       #self.next = self.next.next
-       #move again O(n)
-
-       #return 0 if nothing in list
-       
-       #queue = Dict{}
-       if not head:
-         return
-       
-       #Tip of our head
-       temp = head
-
-
-       while head.next:
-         #Try dictionary implementation
-         #qe[''] = head.nextueu
-
-
-         if head.val == head.next.val:
-            head.next = head.next.next
-
-         else:
-            head = head.next
-    
-       return temp
-
-
-
-
-
-       
-
-
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        current = head
+        while current is not None and current.next is not None:
+            if current.next.val == current.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
