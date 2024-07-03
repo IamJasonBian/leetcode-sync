a4e3eb6f72234ee6b985d233818f7ac92e29d69f
Jason Bian <jasonzb@umich.edu>
2024-07-03 18:31:20 +0000

    [LeetCode Sync] Runtime - 833 ms (22.72%), Memory - 30.1 MB (78.98%)

---
+++ solutions/0123-best-time-to-buy-and-sell-stock-iii/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        k = 2
+        if 2*k >= len(prices): 
+            return sum(max(0, prices[i]-prices[i-1]) for i in range(1, len(prices)))
+        
+        
+        # Note that the DPs here are the best possible implementation for that specific value key at that point in time
+        
+        pnl = [0]*len(prices)
+        for _ in range(k):
+            val = 0
+            for i in range(1, len(pnl)): 
+                val = max(pnl[i], val + prices[i] - prices[i-1]) 
+                pnl[i] = max(pnl[i-1], val)
+        return pnl[-1]
