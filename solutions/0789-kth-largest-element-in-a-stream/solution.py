da2295793cf97081d30eba838db23d9cf8ed3264
Jason Bian <jasonzb@umich.edu>
2024-07-06 20:37:53 +0000

    [LeetCode Sync] Runtime - 83 ms (8.10%), Memory - 20.8 MB (100.00%)

--- solutions/0789-kth-largest-element-in-a-stream/solution.py
+++ solutions/0789-kth-largest-element-in-a-stream/solution.py
@@ -1,27 +1,23 @@
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
-import heapq
-
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
-        self.nums = nums
-        self.k = k
-
-        #Reflexive on self.nums object
-        heapify(self.nums) 
-
-        while len(self.nums) > self.k:
-            heappop(self.nums)
-        
+        self.min=[]
+        self.max=[]
+        self.k=k
+        heapify(self.min)
+        heapify(self.max)
+        for v in nums:
+            heappush(self.max,-v)
+        while self.max and len(self.min)<k:
+            heappush(self.min,-heappop(self.max))
     def add(self, val: int) -> int:
-        if len(self.nums) < self.k:
-            heappush(self.nums, val)
-        elif val > self.nums[0]:
-            heapreplace(self.nums, val)
-        
-        return self.nums[0]
+        if len(self.min)<self.k:
+            heappush(self.min,val)
+        else:
+            if self.max and -self.max[0]>=val:
+                heappush(self.max,-val)
+            else:
+                heappush(self.min,val)
+                if len(self.min)>self.k:
+                    heappush(self.max,-heappop(self.min))
+        return self.min[0]
 
-
-
-    
