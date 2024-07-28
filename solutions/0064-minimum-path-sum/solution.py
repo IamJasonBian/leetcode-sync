6f84feeb846b100b3a594cb58837201014cd27f2
Jason Bian <jasonzb@umich.edu>
2024-07-28 19:22:13 +0000

    [LeetCode Sync] Runtime - 87 ms (5.85%), Memory - 17.4 MB (100.00%)

---
+++ solutions/0064-minimum-path-sum/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        dp = [0 for _ in range(len(grid[0]))]
+        for i in range(len(grid) - 1, -1, -1):
+            for j in range(len(grid[0]) - 1, -1, -1):
+                if i == len(grid) - 1 and j != len(grid[0]) - 1:
+                    dp[j] = grid[i][j] + dp[j + 1]
+                elif j == len(grid[0]) - 1 and i != len(grid) - 1:
+                    dp[j] = grid[i][j] + dp[j]
+                elif i != len(grid) - 1 and j != len(grid[0]) - 1:
+                    dp[j] = grid[i][j] + min(dp[j], dp[j + 1])
+                else:
+                    dp[j] = grid[i][j]
+        return dp[0]
