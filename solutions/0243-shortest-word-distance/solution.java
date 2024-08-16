f52e7ceb5b72450ab69a3a928a7756175cc4179c
Jason Bian <jasonzb@umich.edu>
2024-08-16 19:34:17 +0000

    [LeetCode Sync] Runtime - 3 ms (26.27%), Memory - 46.2 MB (18.86%)

---
+++ solutions/0243-shortest-word-distance/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+
+            /* note two passes would be needed here */
+            if (words[i].equals(word1)) {
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(word2)) {
+                        minDistance = Math.min(minDistance, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDistance;
+    }
+}
