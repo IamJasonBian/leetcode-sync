74db78ddea8870753acbf92581ccf19ed58ee2a5
Jason Bian <jasonzb@umich.edu>
2024-09-25 14:27:45 +0000

    [LeetCode Sync] Runtime - 39 ms (0.08%), Memory - 16.5 MB (100.00%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.py
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # Mapping from digits to letters
+        if not digits:
+            return []
+        
         digit_to_letters = {
             '2': 'abc',
             '3': 'def',
@@ -9,22 +11,18 @@ class Solution:
             '6': 'mno',
             '7': 'pqrs',
             '8': 'tuv',
-            '9': 'wxyz'
+            '9': 'wxyz',
         }
-        
-        # Base case: if digits is empty, return an empty list
-        if not digits:
-            return []
-        
-        # Recursive case: generate combinations for the rest of the string
-        if len(digits) == 1:
-            return list(digit_to_letters[digits[0]])
-        
-        # Combine letters for the current digit with combinations for the rest of the string
-        combinations = []
-        for letter in digit_to_letters[digits[0]]:
-            for combination in self.letterCombinations(digits[1:]):
-                combinations.append(letter + combination)
-        
-        return combinations
 
+        def backtrack(idx, comb):
+            if idx == len(digits):
+                res.append(comb[:])
+                return
+            
+            for letter in digit_to_letters[digits[idx]]:
+                backtrack(idx + 1, comb + letter)
+
+        res = []
+        backtrack(0, "")
+
+        return res
