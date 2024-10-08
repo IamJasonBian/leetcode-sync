6aaf52f0e831e062d70b23c6cff97e34405d7baf
Jason Bian <jasonzb@umich.edu>
2024-10-08 19:12:32 +0000

    [LeetCode Sync] Runtime - 161 ms (9.57%), Memory - 28.1 MB (5.60%)

---
+++ solutions/0189-rotate-array/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        count = 0
+        
+        for start in range(n):
+            if count >= n:
+                break
+            
+            current = start
+            prev = nums[start]
+            
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                
+                if start == current:
+                    break
