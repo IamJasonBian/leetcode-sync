d9d9a963ce70844d9e5f9385114d9893cd5a65b9
Jason Bian <jasonzb@umich.edu>
2024-10-26 18:24:05 +0000

    [LeetCode Sync] Runtime - 151 ms (89.70%), Memory - 17.7 MB (100.00%)

--- solutions/0907-koko-eating-bananas/solution.py
+++ solutions/0907-koko-eating-bananas/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+        
+        def minEatingSpeed(piles: List[int], H: int) -> int:
+            def feasible(speed) -> bool:
+                #return sum(math.ceil(pile / speed) for pile in piles) <= H  # slower        
+                return sum((pile - 1) // speed + 1 for pile in piles) <= H  # faster
 
-        while l <= r:
-            k = (l+r) //2
-            hours = 0
-            for p in piles:
-                hours += math.ceil(p/k)
+            left, right = 1, max(piles)
+            while left < right:
+                mid = left  + (right - left) // 2
+                if feasible(mid):
+                    right = mid
+                else:
+                    left = mid + 1
+            return left
 
-            if hours <= h:
-                res = min(res, k)
-                r = k - 1
-            else:
-                l = k + 1
-        return res
+
+        return minEatingSpeed(piles, h)
