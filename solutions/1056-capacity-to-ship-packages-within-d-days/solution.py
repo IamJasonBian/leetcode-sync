d6e6bcfc3c94e1513561c23367020864d0b0115a
Jason Bian <jasonzb@umich.edu>
2024-10-26 22:26:10 +0000

    [LeetCode Sync] Runtime - 151 ms (93.74%), Memory - 21.2 MB (99.96%)

---
+++ solutions/1056-capacity-to-ship-packages-within-d-days/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shipWithinDays(self, weights: list[int], days: int) -> int:
+
+        def feasible(capacity) -> bool:
+            D = 1
+            total = 0
+            for weight in weights:
+                total += weight
+                if total > capacity:  # too heavy, wait for the next day
+                    total = weight
+                    D += 1
+                    if D > days:  # cannot ship within D days
+                        return False
+            return True
+
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+
+        
