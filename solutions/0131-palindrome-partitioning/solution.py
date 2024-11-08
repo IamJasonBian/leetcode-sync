1c58297b06e2cc9c989f2cbaf3d6da70027e1587
Jason Bian <jasonzb@umich.edu>
2024-11-08 23:14:55 +0000

    [LeetCode Sync] Runtime - 51 ms (36.74%), Memory - 34 MB (51.80%)

--- solutions/0131-palindrome-partitioning/solution.py
+++ solutions/0131-palindrome-partitioning/solution.py
@@ -1,18 +1,8 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-        self.dfs(s, [], result)
-        return result
+        def backtrack(start, path): 
+            if start == len(s): result.append(path); return
+            for end in range(start + 1, len(s) + 1): 
+                if s[start:end] == s[start:end][::-1]: backtrack(end, path + [s[start:end]])
 
-    def isPalindrome(self, s: str) -> bool:
-        return s == s[::-1]
-
-    def dfs(self, s: str, path: List[str], result: List[List[str]]):
-        if not s:
-            result.append(path)
-            return
-        for i in range(1, len(s) + 1):
-            if self.isPalindrome(s[:i]):
-                # add current substring in the currentList
-                self.dfs(s[i:], path + [s[:i]], result)
-                # backtrack and remove the current substring from currentList
+        result = []; backtrack(0, []); return result
