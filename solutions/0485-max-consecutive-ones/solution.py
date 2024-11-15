2383523e2696d7e93161485818d1aa3e635c020c
Jason Bian <jasonzb@umich.edu>
2024-11-15 03:32:38 +0000

    [LeetCode Sync] Runtime - 16 ms (56.94%), Memory - 19.2 MB (99.99%)

--- solutions/0485-max-consecutive-ones/solution.py
+++ solutions/0485-max-consecutive-ones/solution.py
@@ -1,10 +1,50 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        c = mc = 0
-        for num in nums:
-            if num == 1:
-                c += 1
+
+        '''
+
+            The **CRUX** of https://leetcode.com/problems/max-consecutive-ones/description/ is in the fact that below previously came from a position of data structures having greedy optimal substructure. This means that the 
+
+            **original** “In an array of 0s and 1s, we are to fing length of the longest chain of 1s” 
+
+            statement traverses the whole array one to find the various chains of 1. 
+            
+            sequences = []  
+            cnt = 0
+            
+        '''
+
+        sequences = []
+        cnt = 0
+        
+        # 70.61, 17.40
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cnt += 1
+            elif cnt > 0:  # 
+                sequences.append(cnt)
+                cnt = 0
+        
+        if cnt > 0:
+            sequences.append(cnt)
+            
+        return max(sequences) if sequences else 0
+            
+            
+
+        '''
+
+        cnt = 0      # 70.61, 36.58
+        cnt_max = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 1: 
+                cnt += 1    
             else:
-                mc = max(mc, c)
-                c = 0
-        return max(mc, c)
+                cnt_max = max(cnt, cnt_max)  
+                cnt = 0
+
+        return max(cnt, cnt_max)  
+        '''
+       
