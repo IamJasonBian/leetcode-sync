c110f73f0891dca0da82baafef9066eb80c16e91
Jason Bian <jasonzb@umich.edu>
2024-11-15 02:17:53 +0000

    [LeetCode Sync] Runtime - 745 ms (43.21%), Memory - 17.1 MB (99.97%)

--- solutions/0322-coin-change/solution.py
+++ solutions/0322-coin-change/solution.py
@@ -5,11 +5,27 @@ class Solution:
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
 
+
+        '''
+                  |1|4|6,7,8,20|
+                /     \        \
+         |7|20|[]|  |1|6|7,8,20|  |30|20|[]|
+                    /        \
+            |7|20|[]|    |1|7|8,20|
+                         /        \
+                 |7|20|[]|    |1|8|20|
+                               /      \
+                       |7|20|[]|  |1|20|[]|
+        '''
+
+
         for a in range(1, amount + 1):
             for c in coins:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a-c])
 
+
+        
         return dp[amount] if dp[amount] != amount + 1 else -1
 
 
