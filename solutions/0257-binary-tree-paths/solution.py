7823ec66c0271d6bc499e17305ef7a23941ad732
Jason Bian <jasonzb@umich.edu>
2024-11-23 07:58:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.6 MB (100.00%)

---
+++ solutions/0257-binary-tree-paths/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        
+        paths = []
+        stack = [(root, str(root.val))]
+
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        
+        return paths
