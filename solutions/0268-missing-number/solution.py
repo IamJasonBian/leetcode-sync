dc8ae790ba19f020a0df2a4eefe1f6afa2472c39
Jason Bian <jasonzb@umich.edu>
2024-11-23 21:53:05 +0000

    [LeetCode Sync] Runtime - 7 ms (36.64%), Memory - 17.6 MB (100.00%)

---
+++ solutions/0268-missing-number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        '''
+
+            
+
+
+
+        '''
+
+        res = len(nums)
+        for i in range(len(nums)):
+            res += (i - nums[i])
+
+        return res
