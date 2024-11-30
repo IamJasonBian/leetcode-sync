618535918f792dd6e940950c8079b317d52115b2
Jason Bian <jasonzb@umich.edu>
2024-11-30 20:45:41 +0000

    [LeetCode Sync] Runtime - 2 ms (98.01%), Memory - 17.4 MB (87.43%)

--- solutions/1130-last-stone-weight-ii/solution.py
+++ solutions/1130-last-stone-weight-ii/solution.py
@@ -1,17 +1,10 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        stonesum = sum(stones)
-        half = stonesum // 2
-        n = len(stones)
-        dp = [[0 for j in range(half+1)]for _ in range(n)]
-
-        for j in range(stones[0], half+1):
-            dp[0][j] = stones[0]
-
-        for i in range(1, n):
-            for j in range(0, half+1):
-                if j < stones[i]:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-stones[i]]+stones[i])
-        return stonesum - dp[n-1][half] * 2
+        prev_state = {0}
+        for elem in stones:
+            state = set()
+            for prev in prev_state:
+                state.add(elem + prev)
+                state.add(abs(prev - elem))
+            prev_state = state
+        return min(prev_state)
