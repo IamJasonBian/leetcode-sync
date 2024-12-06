f68c7171a590f23861cdd4f04045738d5b41bcb5
Jason Bian <jasonzb@umich.edu>
2024-12-06 19:21:25 +0000

    [LeetCode Sync] Runtime - 12 ms (82.37%), Memory - 19.4 MB (99.94%)

--- solutions/0135-candy/solution.py
+++ solutions/0135-candy/solution.py
@@ -1,24 +1,12 @@
-class Solution(object):
-    def candy(self, ratings):
-        """
-        :type ratings: List[int]
-        :rtype: int
-        """
-        n = len(ratings)
-        # Initialize candies array with 1 candy for each child
-        candies = [1] * n
-        
-        # First pass: Left to Right
+class Solution:
+    def candy(self, ratings) -> int:
+        n = len(ratings)  
+        candies = [1] * n  
         for i in range(1, n):
-            if ratings[i] > ratings[i-1]:
-                candies[i] = candies[i-1] + 1
-        
-        # Second pass: Right to Left
-        for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
-                candies[i] = candies[i+1] + 1
-        
-        # Return the total number of candies
-        return sum(candies)
-
-
+            if ratings[i - 1] < ratings[i] and candies[i - 1] >= candies[i]:
+                candies[i] = candies[i - 1] + 1
+        for i in range(n - 2, -1, -1):
+            if ratings[i + 1] < ratings[i] and candies[i + 1] >= candies[i]:
+                candies[i] = candies[i + 1] + 1
+        total_candies = sum(candies) 
+        return total_candies
