a0223ccfe9e115a60db701f0f8ec0cbbbe53f23f
Jason Bian <jasonzb@umich.edu>
2024-12-16 06:25:14 +0000

    [LeetCode Sync] Runtime - 1317 ms (18.91%), Memory - 21.3 MB (86.53%)

--- solutions/1713-dot-product-of-two-sparse-vectors/solution.py
+++ solutions/1713-dot-product-of-two-sparse-vectors/solution.py
@@ -1,13 +1,24 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.nonzeros = {}
-        for i, n in enumerate(nums):
-            if n != 0:
-                self.nonzeros[i] = n              
+        self.pairs = []
+        for index, value in enumerate(nums):
+            if value != 0:
+                self.pairs.append([index, value])
 
     def dotProduct(self, vec: 'SparseVector') -> int:
         result = 0
-        for i, n in self.nonzeros.items():
-            if i in vec.nonzeros:
-                result += n * vec.nonzeros[i]
+        p, q = 0, 0
+
+        while p < len(self.pairs) and q < len(vec.pairs):
+            if self.pairs[p][0] == vec.pairs[q][0]:
+                result += self.pairs[p][1] * vec.pairs[q][1]
+                # we're just traversing both pointers (no gimmick each pointer is just keeping track of array), this isn't that deep
+                p += 1
+                q += 1
+            elif self.pairs[p][0] < vec.pairs[q][0]:
+                p += 1
+            else:
+                q += 1
+
         return result
+
