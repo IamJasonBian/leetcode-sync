84a7ff3904018cb66bcb6f249294736506ba7ea9
Jason Bian <jasonzb@umich.edu>
2024-12-16 01:20:18 +0000

    [LeetCode Sync] Runtime - 158 ms (64.26%), Memory - 17.7 MB (96.37%)

--- solutions/1171-shortest-path-in-binary-matrix/solution.py
+++ solutions/1171-shortest-path-in-binary-matrix/solution.py
@@ -5,8 +5,7 @@ class Solution:
         max_col = len(grid[0]) - 1
         directions = [
             (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        
-        # Helper function to find the neighbors of a given cell.
+
         def get_neighbours(row, col):
             for row_difference, col_difference in directions:
                 new_row = row + row_difference
@@ -17,24 +16,21 @@ class Solution:
                     continue
                 yield (new_row, new_col)
         
-        # Check that the first and last cells are open. 
         if grid[0][0] != 0 or grid[max_row][max_col] != 0:
             return -1
-        
-        # Set up the BFS.
+
         queue = deque()
         queue.append((0, 0))
         grid[0][0] = 1 
         
-        # Carry out the BFS.
         while queue:
             row, col = queue.popleft()
             distance = grid[row][col]
             if (row, col) == (max_row, max_col):
-                return distance
-            for neighbour_row, neighbour_col in get_neighbours(row, col):
-                grid[neighbour_row][neighbour_col] = distance + 1
-                queue.append((neighbour_row, neighbour_col))
+               return distance
+
+            for neighbour_row, neightbour_col in get_neighbours(row, col):
+                grid[neighbour_row][neightbour_col] = distance + 1
+                queue.append((neighbour_row, neightbour_col))
         
-        # There was no path.
         return -1
