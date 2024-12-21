d90e3d958c1188753e26a4d202ae99010a2b23dd
Jason Bian <jasonzb@umich.edu>
2024-12-21 20:02:54 +0000

    [LeetCode Sync] Runtime - 5 ms (43.39%), Memory - 18.2 MB (72.01%)

--- solutions/0406-queue-reconstruction-by-height/solution.py
+++ solutions/0406-queue-reconstruction-by-height/solution.py
@@ -1,13 +1,9 @@
-class Solution:
-    # O(n^2) time | O(n) space
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        res = []
-
-        # Sort people by height and position in the queue
-        # Key: (-height, k) to descending height and ascending k
+class Solution(object):
+    def reconstructQueue(self, people):
         people.sort(key=lambda x: (-x[0], x[1]))
 
-        for p in people:
-            _, position = p
-            res.insert(position, p)
-        return res
+        result = []
+        for person in people:
+            result.insert(person[1], person)
+
+        return result
