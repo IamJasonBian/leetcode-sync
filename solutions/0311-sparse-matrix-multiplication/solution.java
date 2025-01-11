a155be2abdbfa9d1a3b55189b55831ec05a1a1b3
Jason Bian <jasonzb@umich.edu>
2025-01-11 00:19:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (99.81%)

--- solutions/0311-sparse-matrix-multiplication/solution.java
+++ solutions/0311-sparse-matrix-multiplication/solution.java
@@ -1,53 +1,23 @@
 class Solution {
     public int[][] multiply(int[][] mat1, int[][] mat2) {
-        int m = mat1.length;
+        int n = mat1.length;
         int k = mat1[0].length;
-        int n = mat2[0].length;
+        int m = mat2[0].length;
         
-        // Convert matrices to sparse representation
-        Map<Integer, Integer>[] sparseRows = new Map[m];
-        Map<Integer, Integer>[] sparseCols = new Map[n];
+        // Product matrix will have 'n x m' size.
+        int[][] ans = new int[n][m];
         
-        // Initialize maps
-        for (int i = 0; i < m; i++) {
-            sparseRows[i] = new HashMap<>();
-            for (int j = 0; j < k; j++) {
-                if (mat1[i][j] != 0) {
-                    sparseRows[i].put(j, mat1[i][j]);
+        for (int rowIndex = 0; rowIndex < n; ++rowIndex) {
+            for (int elementIndex = 0; elementIndex < k; ++elementIndex) {
+                // If current element of mat1 is non-zero then iterate over all columns of mat2.
+                if (mat1[rowIndex][elementIndex] != 0)  {
+                    for (int colIndex = 0; colIndex < m; ++colIndex) {
+                        ans[rowIndex][colIndex] += mat1[rowIndex][elementIndex] * mat2[elementIndex][colIndex];
+                    }
                 }
             }
         }
         
-        for (int j = 0; j < n; j++) {
-            sparseCols[j] = new HashMap<>();
-            for (int i = 0; i < k; i++) {
-                if (mat2[i][j] != 0) {
-                    sparseCols[j].put(i, mat2[i][j]);
-                }
-            }
-        }
-        
-        // Compute result
-        int[][] result = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[i][j] = dotProduct(sparseRows[i], sparseCols[j]);
-            }
-        }
-        
-        return result;
-    }
-    
-    private int dotProduct(Map<Integer, Integer> vec1, Map<Integer, Integer> vec2) {
-        if (vec2.size() < vec1.size()) {
-            var tmp = vec1;
-            vec1 = vec2;
-            vec2 = tmp;
-        }
-        int product = 0;
-        for (var entry : vec1.entrySet()) {
-            product += vec2.getOrDefault(entry.getKey(), 0) * entry.getValue();
-        }
-        return product;
+        return ans;
     }
 }
