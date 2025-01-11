69225b3b91190198a9e2dd902b75c36a4627da1c
Jason Bian <jasonzb@umich.edu>
2025-01-11 00:29:48 +0000

    [LeetCode Sync] Runtime - 3 ms (39.96%), Memory - 18.2 MB (85.58%)

--- solutions/0311-sparse-matrix-multiplication/solution.py
+++ solutions/0311-sparse-matrix-multiplication/solution.py
@@ -1,11 +1,27 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        ans = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+        def compress_matrix(matrix: List[List[int]]) -> List[List[int]]:
+            rows, cols = len(matrix), len(matrix[0])
+            compressed_matrix = [[] for _ in range(rows)]
+            for row in range(rows):
+                for col in range(cols):
+                    if matrix[row][col]:
+                        compressed_matrix[row].append([matrix[row][col], col])
+            return compressed_matrix
         
-        for row_index, row_elements in enumerate(mat1):
-            for element_index, row_element in enumerate(row_elements):
-                if row_element:
-                    for col_index, col_element in enumerate(mat2[element_index]):
-                        ans[row_index][col_index] += row_element * col_element
+        m = len(mat1)
+        k = len(mat1[0])
+        n = len(mat2[0])
         
+        A = compress_matrix(mat1)
+        B = compress_matrix(mat2)
+        
+        ans = [[0] * n for _ in range(m)]
+        
+        for mat1_row in range(m):
+            for element1, mat1_col in A[mat1_row]:
+                # The index one is used to string the idx in the other matrix
+                for element2, mat2_col in B[mat1_col]:
+                    ans[mat1_row][mat2_col] += element1 * element2
+                    
         return ans
