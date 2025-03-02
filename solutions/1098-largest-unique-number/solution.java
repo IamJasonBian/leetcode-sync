71986d93634dadbf5729c557a023bae4883e9f5f
Jason Bian <jasonzb@umich.edu>
2025-03-02 01:47:30 +0000

    [LeetCode Sync] Runtime - 2 ms (88.32%), Memory - 42.4 MB (100.00%)

---
+++ solutions/1098-largest-unique-number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestUniqueNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; i--){
+            if ((i==n-1 || nums[i] != nums[i+1]) && (i == 0 || nums[i] != nums[i-1])){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
