b3cd9973a60c8dd352d99ff70bf5a0b3332ecfa2
Jason Bian <jasonzb@umich.edu>
2025-03-16 23:43:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (84.02%)

--- solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -1,38 +1,23 @@
-from typing import List, Optional
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        
-        root = TreeNode(preorder[0])
-        
-        if len(preorder) == 1:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+
+        def array_to_tree(left, right):
+            nonlocal preorder_index
+            if left > right:
+                return None
+            root_value = preorder[preorder_index]
+            root = TreeNode(root_value)
+
+            preorder_index += 1
+            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
+            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+
             return root
-        
-        # Find the root in the inorder traversal
-        rootIndexInorder = inorder.index(preorder[0])
-        
-        # The elements before the root in the inorder traversal are in the left subtree
-        # The elements after the root in the inorder traversal are in the right subtree
-        leftSubtreeInorder = inorder[:rootIndexInorder]
-        rightSubtreeInorder = inorder[rootIndexInorder+1:]
-        
-        # The elements after the root in the preorder traversal are in the right subtree
-        # The elements before the root in the preorder traversal are in the left subtree
-        leftSubtreePreorder = preorder[1:len(leftSubtreeInorder)+1]
-        rightSubtreePreorder = preorder[len(leftSubtreeInorder)+1:]
-        
-        root.left = self.buildTree(leftSubtreePreorder, leftSubtreeInorder)
-        root.right = self.buildTree(rightSubtreePreorder, rightSubtreeInorder)
-        
-        return root
 
+        preorder_index = 0
 
+        inorder_index_map = {}
+        for index, value in enumerate(inorder):
+            inorder_index_map[value] = index
+
+        return array_to_tree(0, len(preorder) - 1)
