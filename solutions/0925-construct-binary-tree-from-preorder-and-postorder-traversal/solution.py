8e36e20dc347d82a4fbba7ae122432c45fe41459
Jason Bian <jasonzb@umich.edu>
2025-03-30 00:49:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (68.20%)

--- solutions/0925-construct-binary-tree-from-preorder-and-postorder-traversal/solution.py
+++ solutions/0925-construct-binary-tree-from-preorder-and-postorder-traversal/solution.py
@@ -1,48 +1,21 @@
 class Solution:
+    def __init__(self):
+        self.pre_index = 0
+        self.post_index = 0
+
     def constructFromPrePost(
         self, preorder: List[int], postorder: List[int]
     ) -> Optional[TreeNode]:
-        num_of_nodes = len(preorder)
-        index_in_post_order = [0] * (num_of_nodes + 1)
-        for index in range(num_of_nodes):
-            index_in_post_order[postorder[index]] = index
+        return self._construct_tree(preorder, postorder)
 
-        return self._construct_tree(
-            0, num_of_nodes - 1, 0, preorder, index_in_post_order
-        )
     def _construct_tree(
-        self,
-        pre_start: int,
-        pre_end: int,
-        post_start: int,
-        preorder: List[int],
-        index_in_post_order: List[int],
+        self, preorder: List[int], postorder: List[int]
     ) -> Optional[TreeNode]:
-
-        if pre_start > pre_end:
-            return None
-
-
-        if pre_start == pre_end:
-            return TreeNode(preorder[pre_start])
-
-        left_root = preorder[pre_start + 1]
-        num_of_nodes_in_left = index_in_post_order[left_root] - post_start + 1
-
-        root = TreeNode(preorder[pre_start])
-        root.left = self._construct_tree(
-            pre_start + 1,
-            pre_start + num_of_nodes_in_left,
-            post_start,
-            preorder,
-            index_in_post_order,
-        )
-        root.right = self._construct_tree(
-            pre_start + num_of_nodes_in_left + 1,
-            pre_end,
-            post_start + num_of_nodes_in_left,
-            preorder,
-            index_in_post_order,
-        )
-
+        root = TreeNode(preorder[self.pre_index])
+        self.pre_index += 1
+        if root.val != postorder[self.post_index]:
+            root.left = self._construct_tree(preorder, postorder)
+        if root.val != postorder[self.post_index]:
+            root.right = self._construct_tree(preorder, postorder)
+        self.post_index += 1
         return root
