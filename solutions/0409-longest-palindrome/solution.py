ad7fc6d7643cc8e0236979992caa671e3442219b
Jason Bian <jasonzb@umich.edu>
2025-03-31 14:19:00 +0000

    [LeetCode Sync] Runtime - 1 ms (60.12%), Memory - 18.1 MB (63.64%)

---
+++ solutions/0409-longest-palindrome/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        x = Counter(s)
+        total = 0
+        checkodd = False
+
+        for i in x.values():
+            if i % 2 == 0:
+                total += i
+            elif not checkodd:
+                total += i
+                checkodd = True
+            else:
+                total += i-1
+        return total
+
+        
