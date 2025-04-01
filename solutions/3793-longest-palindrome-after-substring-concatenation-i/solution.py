6985835be553ad4244fbdd5bff691243088588b9
Jason Bian <jasonzb@umich.edu>
2025-04-01 05:18:29 +0000

    [LeetCode Sync] Runtime - 1307 ms (40.23%), Memory - 18 MB (78.16%)

--- solutions/3793-longest-palindrome-after-substring-concatenation-i/solution.py
+++ solutions/3793-longest-palindrome-after-substring-concatenation-i/solution.py
@@ -1,8 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
+
         max_length = 0  
 
+        '''
 
+            strings: s and t
+                new string: select a substring from s and a substring from t, then concatenating them in order
+
+            Return the length of the longest palindrome that can be formed
+
+        '''
 
         
         for i in range(len(s) + 1):
@@ -10,8 +18,6 @@ class Solution:
                 s_substr = s[i:j]
                 for k in range(len(t) + 1):
                     for l in range(k, len(t) + 1):
-
-
                         
                         t_substr = t[k:l]
                         concat = s_substr + t_substr
