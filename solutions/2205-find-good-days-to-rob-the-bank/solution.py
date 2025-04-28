d43a5d108322398747d9f0e050e5bf3e67cedfd6
Jason Bian <jasonzb@umich.edu>
2025-04-28 04:24:00 +0000

    [LeetCode Sync] Runtime - 51 ms (99.71%), Memory - 33.1 MB (96.79%)

--- solutions/2205-find-good-days-to-rob-the-bank/solution.py
+++ solutions/2205-find-good-days-to-rob-the-bank/solution.py
@@ -1,5 +1,31 @@
 class Solution:
-    def goodDaysToRobBank(self, a: List[int], t: int) -> List[int]:
-        f = lambda a:[*accumulate(pairwise(a),lambda q,p:(q+1)*ge(*p),initial=0)]
-        l, r = f(a), f(a[::-1])[::-1]
-        return [i for i in range(len(a)) if l[i]>=t<=r[i]]
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        n = len(security)
+        if time == 0:
+            return list(range(n))
+        
+        non_increasing = [False] * n
+        dec_len = 1
+        
+        # First pass: Check non-increasing condition
+        for idx in range(1, n):
+            if security[idx-1] >= security[idx]:
+                if dec_len >= time:
+                    non_increasing[idx] = True
+                dec_len += 1
+            else:
+                dec_len = 1
+        
+        inc_len = 1
+        good_days = []
+        
+        # Second pass: Check non-decreasing condition
+        for idx in range(n-2, -1, -1):
+            if security[idx] <= security[idx + 1]:
+                if inc_len >= time and non_increasing[idx]:
+                    good_days.append(idx)
+                inc_len += 1
+            else:
+                inc_len = 1
+        
+        return good_days
