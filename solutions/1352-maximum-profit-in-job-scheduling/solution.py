4881768974ba4b34a4febfd9337afe868ab573dd
Jason Bian <jasonzb@umich.edu>
2025-06-25 12:39:39 +0000

    [LeetCode Sync] Runtime - 283 ms (26.59%), Memory - 32.3 MB (82.23%)

--- solutions/1352-maximum-profit-in-job-scheduling/solution.py
+++ solutions/1352-maximum-profit-in-job-scheduling/solution.py
@@ -1,34 +1,46 @@
-import heapq
-from typing import List
+class Solution(object):
+    def jobScheduling(self, startTime, endTime, profit):
 
-class Job:
-    def __init__(self, start: int, end: int, profit: int):
-        self.start = start
-        self.end = end
-        self.profit = profit
+        dp = [0]
+        cur_end = [0] 
+        n = len(endTime)       
 
-class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted([Job(s, e, p) for s, e, p in zip(startTime, endTime, profit)], 
-                     key=lambda x: x.end)
-        
-        
-        dp = [(0, 0)]  
-        
-        for job in jobs:
-           
-            left, right = 0, len(dp)
-            while left < right:
-                mid = (left + right) // 2
-                if dp[mid][0] <= job.start:
-                    left = mid + 1
+        #Binary search to get the closest non overlapping job
+        def BinarySearch(v):
+            l = 0
+            r = len(cur_end)-1
+
+            ans = 0
+            while l <= r:
+                mid = l + (r-l)//2
+
+                if cur_end[mid]<=v:
+                    ans = mid
+                    l = mid+1
                 else:
-                    right = mid
-            
-            max_profit_before = dp[right - 1][1] if right > 0 else 0
-            current_profit = max_profit_before + job.profit
-            
-            if not dp or current_profit > dp[-1][1]:
-                dp.append((job.end, current_profit))
+                    r = mid-1
+            return ans
+
+        # Sort based on the endtime
+        jobs = [
+            [endTime[i],startTime[i],profit[i]] for i in range(n)
+        ]
+        jobs.sort()
+
+
+        for i in range(n):
+            e,s,p = jobs[i]
         
-        return dp[-1][1] if dp else 0
+
+            idx = BinarySearch(s)
+            
+            # Is prev overlapping job more profitable or the this one with non overlapping job?
+            dp.append(
+                max(
+                    p + dp[idx],
+                    dp[-1]
+                )
+            )
+          
+            cur_end.append(e)
+        return dp[-1]
