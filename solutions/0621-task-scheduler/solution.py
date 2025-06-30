6418141b049a2d24c0aaf384bcce93951a6e8b07
Jason Bian <jasonzb@umich.edu>
2025-06-30 04:58:45 +0000

    [LeetCode Sync] Runtime - 19 ms (83.57%), Memory - 19.2 MB (94.50%)

--- solutions/0621-task-scheduler/solution.py
+++ solutions/0621-task-scheduler/solution.py
@@ -1,32 +1,13 @@
-import heapq as hq
+from collections import Counter
+from typing import List
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # We need to permuate through each task and cycle to calculate the difference in permutations
-        count = Counter(tasks)
-
-        #Array implementation of maxHeap 
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
-
-        time = 0
-        q = deque() # pairs of [-cnt, idleTime]
-
-        while maxHeap or q: 
-
-            #processing -> increment time by 1
-            time += 1
-
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-
-                    #time + n (backoff time)
-                    q.append([cnt, time + n])
-
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
         
-        return time
         
+        task_counts = Counter(tasks)
+        max_freq = max(task_counts.values())
+        max_count = list(task_counts.values()).count(max_freq)
 
+        min_intervals = (max_freq - 1) * (n + 1) + max_count
+        return max(min_intervals, len(tasks))
