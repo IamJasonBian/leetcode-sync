3b6e4408cbd49b5ced00e5ceab0e4341e07f558b
Jason Bian <jasonzb@umich.edu>
2025-07-06 21:32:17 +0000

    [LeetCode Sync] Runtime - 83 ms (6.50%), Memory - 63.8 MB (99.96%)

--- solutions/0287-find-the-duplicate-number/solution.java
+++ solutions/0287-find-the-duplicate-number/solution.java
@@ -1,23 +1,32 @@
-class Solution {
+public class Solution {
+	int hashes=5;
+	int bits=5000000;
+	int[] primes = {31, 37,  41,  43, 47, 53, 59, 61, 67, 71};
+    
     public int findDuplicate(int[] nums) {
         
-        // Find the intersection point of the two runners.
-        int tortoise = nums[0];
-        int hare = nums[0];
-        
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while (tortoise != hare);
+        boolean[] bitsMasks = new boolean[bits];
 
-        // Find the "entrance" to the cycle.
-        tortoise = nums[0];
-        
-        while (tortoise != hare) {
-            tortoise = nums[tortoise];
-            hare = nums[hare];
-        }
 
-        return hare;
+
+        // bloom filter: 
+        for(int i=0; i<nums.length; i++){
+        	int count=0;
+            for(int h=0; h<hashes; h++){
+            	String s =Integer.toString(nums[i]);
+                int hash = 0;
+                for(int c=0; c<s.length();c++)
+                	hash = primes[h]*hash + s.charAt(c);
+                hash =  hash % bits;
+                if(hash<0)
+                	hash = ( bits + hash) %bits;
+                if(!bitsMasks[hash])
+                	count++;
+                bitsMasks[hash]=true;
+            }
+            if (count==0)
+            	return nums[i];
+        }       
+        return -1;
     }
 }
