f4ae162f4e88eea751cb6acbbb9074a71531aaef
Jason Bian <jasonzb@umich.edu>
2025-08-17 22:28:23 +0000

    [LeetCode Sync] Runtime - 20 ms (88.50%), Memory - 45.2 MB (93.25%)

--- solutions/2529-range-product-queries-of-powers/solution.py
+++ solutions/2529-range-product-queries-of-powers/solution.py
@@ -1,18 +1,23 @@
-from typing import List
-
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        MOD = 10**9 + 7
+        mod = 10**9 + 7
 
-        powers = []
-        for i in range(n.bit_length()):
-            if n & (1 << i):
-                powers.append(1 << i)
-        
-        answers = []
+        bins, rep = [], 1
+        while n > 0:
+            if n % 2 == 1:
+                bins.append(rep)
+            n //= 2
+            rep *= 2
+
+        m = len(bins)
+        results = [[0] * m for _ in range(m)]
+        for i in range(m):
+            cur = 1
+            for j in range(i, m):
+                cur = cur * bins[j] % mod
+                results[i][j] = cur
+
+        ans = []
         for left, right in queries:
-            prod = 1
-            for i in range(left, right + 1):
-                prod = (prod * powers[i]) % MOD
-            answers.append(prod)
-        return answers
+            ans.append(results[left][right])
+        return ans
