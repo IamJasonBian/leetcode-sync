cdd1d9899b046ba240475384337467134ea1b5a8
Jason Bian <jasonzb@umich.edu>
2025-08-30 22:28:08 +0000

    [LeetCode Sync] Runtime - 3 ms (90.37%), Memory - 20.9 MB (80.36%)

--- solutions/0347-top-k-frequent-elements/solution.py
+++ solutions/0347-top-k-frequent-elements/solution.py
@@ -1,19 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        fmap = Counter(nums)
-        heap = []
-        heapq.heapify(heap)
-        for num, freq in fmap.items():
-            if len(heap) < k:
-                heapq.heappush(heap, (freq, num))
-            elif freq > heap[0][0]: 
-                heapq.heappop(heap)
-                heapq.heappush(heap, (freq, num))
-        return [num for _, num in heap]
 
-# in host memory bullshit
-with open("user.out", "w") as f:
-    inputs = map(loads, stdin)
-    for nums in inputs:
-        print(str(Solution().topKFrequent(nums, next(inputs))).replace(" ", ""), file=f)
-exit(0)
+        freq = {}
+        for i in nums:
+            freq[i] = freq.get(i, 0) + 1
+        sorted_res =  sorted(freq.keys(), key=lambda x: (-freq[x], x))
+        return sorted_res[:k]
