2bb1b5a6282301d89b5ba5c90fe79d18ed45ae4b
Jason Bian <jasonzb@umich.edu>
2025-09-01 08:56:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (85.03%)

--- solutions/0532-k-diff-pairs-in-an-array/solution.py
+++ solutions/0532-k-diff-pairs-in-an-array/solution.py
@@ -2,11 +2,22 @@ from collections import Counter
 
 class Solution:
     def findPairs(self, nums, k):
+
+
+
         result = 0
         counter = Counter(nums)
+
         for x in counter:
             if k > 0 and x + k in counter:
+                # threshold stacking 
+
                 result += 1
+                
             elif k == 0 and counter[x] > 1:
+
+                # nums = [1,3,1,5,4], k = 0 case
+                # in-place diffs etc
+
                 result += 1
         return result
