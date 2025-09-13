b84908b7738e25c7a361bf43fc57cb67bfa2d506
Jason Bian <jasonzb@umich.edu>
2025-09-13 23:42:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.7 MB (76.05%)

--- solutions/0253-meeting-rooms-ii/solution.py
+++ solutions/0253-meeting-rooms-ii/solution.py
@@ -3,24 +3,16 @@ import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
+        
 
-        starts = [s for s, _ in intervals]
-        ends = [e for _, e in intervals]
-        heapq.heapify(starts)
-        heapq.heapify(ends)
 
-        rooms = 0
-        ongoing = 0
-        while starts:
-            if starts[0] < ends[0]:
-                heapq.heappop(starts)
-                ongoing += 1
-                rooms = max(rooms, ongoing)
-            else:
+        free_rooms = []
+        intervals.sort(key = lambda x : x[0])
+        heapq.heappush(free_rooms, intervals[0][1])
 
-                heapq.heappop(ends)
-                ongoing -= 1
+        for i in intervals[1:]: 
+            if free_rooms[0] <= i[0]:
+                heapq.heappop(free_rooms)
+            heapq.heappush(free_rooms, i[1])
+        return len(free_rooms)
 
-        return rooms
