6dd5deecb1158f19207e6459e6989557cc1ab7c1
Jason Bian <jasonzb@umich.edu>
2025-09-22 02:52:54 +0000

    [LeetCode Sync] Runtime - 84 ms (55.32%), Memory - 51.4 MB (6.12%)

---
+++ solutions/0435-non-overlapping-intervals/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals):
+        if not intervals:
+            return 0
+        
+        intervals.sort(key=lambda x: x[1])
+        
+        count = 0
+        last_end = float('-inf')
+        
+        for start, end in intervals:
+            if start >= last_end:
+                last_end = end
+            else:
+                count += 1
+        
+        return count
