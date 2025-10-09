748b431fb1a18241f10d06255e63f602f71ba1bc
Jason Bian <jasonzb@umich.edu>
2025-10-09 06:07:36 +0000

    [LeetCode Sync] Runtime - 20 ms (96.48%), Memory - 30.7 MB (90.11%)

--- solutions/0208-implement-trie-prefix-tree/solution.py
+++ solutions/0208-implement-trie-prefix-tree/solution.py
@@ -1,61 +1,35 @@
-class TrieNode:    
- 
-    #Constructor, Time O(1), Space O(1), 128 is constant
-    def __init__(self, c):
-        self.children = [None]*128 #don't use 26 if there is space or other special characters
-        self.isEnd = False
-        self.data = c
-
-
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode('')
+        self.trie = {}        
 
     def insert(self, word: str) -> None:
-        node = self.root
-        for ch in word:
-            idx = ord(ch)
-            if not node.children[idx]:
-                node.children[idx] = TrieNode(ch)
-            node = node.children[idx]
-        node.isEnd = True
+        d = self.trie
 
-    #returns all words with given prefix
-    #Time O(n), Space O(n), n is number of nodes included(prefix and branches) 
-    def startsWith(self, prefix):
-        node = self.root
-        res = []
-        for ch in prefix:
-            node = node.children[ord(ch)]
-            if node == None:
-                return []     
-        self.helper(node, res, prefix[:-1]) 
-        return res   
+        for char in word:
+            if char not in d:
+                d[char] = {}
+            d = d[char]
+        d['.'] = '.'
 
-    def search(self, word) -> bool:
-        node = self.root
-        for ch in word:
-            idx = ord(ch)
-            if not node.children[idx]:
-                return False # Corrected to use 'False'
-            node = node.children[idx]
-        return node.isEnd # Corrected indentation
+    def search(self, word: str) -> bool:
+        d = self.trie
 
-    #recursion function called by autocomplete, Time O(n), Space O(n)	
-    # n is number of nodes in bra
-    def helper(self, node, res, prefix):
-        if node == None:
-            return
-        if node.isEnd :
-            res.append(prefix + node.data)		   	      	   
-        for child in node.children:	         
-            self.helper(child, res,prefix + node.data)		             
- 
+        for char in word:
+            if char not in d:
+                return False
+            d = d[char]
+        
+        return '.' in d
+
+    def startsWith(self, prefix: str) -> bool:
+        d = self.trie
+
+        for char in prefix:
+            if char not in d:
+                return False
+            d = d[char]
+        
+        return True
 
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
