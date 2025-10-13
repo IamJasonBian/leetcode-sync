04056bcc0fdfcab1e1480ce992a7e9fe3514a4d1
Jason Bian <jasonzb@umich.edu>
2025-10-13 01:31:22 +0000

    [LeetCode Sync] Runtime - 3 ms (34.18%), Memory - 19.9 MB (67.28%)

--- solutions/0098-validate-binary-search-tree/solution.py
+++ solutions/0098-validate-binary-search-tree/solution.py
@@ -1,20 +1,19 @@
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def isValidBST(self, root: TreeNode) -> bool:
         if not root:
             return True
         
         stack = [(root, float('-inf'), float('inf'))]
-        
+
         while stack:
             node, lower, upper = stack.pop()
-            
             if not node:
                 continue
-            
+                
             val = node.val
+
             if val <= lower or val >= upper:
                 return False
             stack.append((node.right, val, upper))
             stack.append((node.left, lower, val))
-        
         return True
