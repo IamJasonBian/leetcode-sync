686aecb0ab74b97dab6c39573ac56fc44228700c
Jason Bian <jasonzb@umich.edu>
2025-10-17 23:44:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (72.13%)

--- solutions/0022-generate-parentheses/solution.py
+++ solutions/0022-generate-parentheses/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-    
-      
         
-        output_ls = []
         
-        def backtrack(output_ls, current_string, open ,close, max_s):
-            if len(current_string) == max_s * 2:
-                output_ls.append(current_string)
-                return
-            if(open < max_s):
-                backtrack(output_ls, current_string + "(", open + 1, close, max_s)
-            if(close < open):
-                backtrack(output_ls, current_string + ")", open, close + 1, max_s)  
-            
-
-        backtrack(output_ls, "", 0, 0, n)
-       
-        return output_ls
+        stack = [("", 0, 0)]
+        result = []
+        while stack:
+            s, o, c = stack.pop()
+            if o == n and c == n:
+                result.append(s)
+                continue
+            if o < n:
+                stack.append((s + '(', o + 1, c))
+            if c < o:
+                stack.append((s + ')', o, c + 1))
+        return result
