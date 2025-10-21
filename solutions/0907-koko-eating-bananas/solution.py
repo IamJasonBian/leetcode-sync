a916f65f025c1c0c37f2ef6786f4230661c6c5a1
Jason Bian <jasonzb@umich.edu>
2025-10-21 01:12:18 +0000

    [LeetCode Sync] Runtime - 159 ms (74.16%), Memory - 19 MB (71.81%)

--- solutions/0907-koko-eating-bananas/solution.py
+++ solutions/0907-koko-eating-bananas/solution.py
@@ -1,23 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
-        def minEatingSpeed(piles: List[int], H: int) -> int:
-            def feasible(speed) -> bool:
-                # keep in mind that sorting is not needed here because everything descends 
-                return sum((pile - 1) // speed + 1 for pile in piles) <= H  # faster
 
             left, right = 1, max(piles)
             while left < right:
-
-                # we are eatting the piles up to a certain direction
+                def feasible(speed) -> bool: 
+                    return sum((pile - 1) // speed + 1 for pile in piles) <= h  
                 
-                mid = left  + (right - left) // 2
+                mid = (left + right) // 2
+            
                 if feasible(mid):
-
                     right = mid
                 else:
                     left = mid + 1
             return left
 
 
-        return minEatingSpeed(piles, h)
