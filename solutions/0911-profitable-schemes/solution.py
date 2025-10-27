fd183a7fd9f54da345cabdb4fec71ff3ad19aa37
Jason Bian <jasonzb@umich.edu>
2025-10-27 00:42:16 +0000

    [LeetCode Sync] Runtime - 809 ms (77.59%), Memory - 18.4 MB (83.61%)

--- solutions/0911-profitable-schemes/solution.py
+++ solutions/0911-profitable-schemes/solution.py
@@ -1,26 +1,9 @@
-from typing import List
-
 class Solution:
-      def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-          MOD = 10**9 + 7
-          current = {(0, 0): 1}
-          for i in range(len(group)):
-              next_layer = {}
-              states = list(current.items())
-
-              for (members, prof), count in states:
-                  key1 = (members, prof)
-                  next_layer[key1] = (next_layer.get(key1, 0) + count) % MOD
-                  if members + group[i] <= n:
-                      new_members = members + group[i]
-                      new_profit = min(prof + profit[i], minProfit)
-                      key2 = (new_members, new_profit)
-                      next_layer[key2] = (next_layer.get(key2, 0) + count) % MOD
-
-              current = next_layer
-          result = 0
-          for (members, prof), count in current.items():
-              if prof >= minProfit:
-                  result = (result + count) % MOD
-
-          return result
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        d =[[0] * (n +1) for i in range(minProfit + 1)]
+        d[0]= [1] * (n +1)
+        for j in range(0, len(group)):
+            for p in range(minProfit, -1, -1):
+                for i in range(n, group[j] -1, -1):  
+                    d[p][i] += d[max(p- profit[j] , 0 )][i - group[j]]         
+        return d[minProfit][n] % (10**9+7)
