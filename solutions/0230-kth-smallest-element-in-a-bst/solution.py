4ed00457e4519820681b9268ad44ac7521b21bee
Jason Bian <jasonzb@umich.edu>
2025-10-28 22:34:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.1 MB (68.71%)

--- solutions/0230-kth-smallest-element-in-a-bst/solution.py
+++ solutions/0230-kth-smallest-element-in-a-bst/solution.py
@@ -8,19 +8,21 @@ class TreeNode:
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        n = 0
+
         stack = []
         cur = root
+        count = 0
 
-        # Corrected the loop condition here
-        while cur or len(stack) > 0:
+            
+        while cur or stack:
             while cur:
                 stack.append(cur)
                 cur = cur.left
-
             cur = stack.pop()
-            n += 1
-            if n == k:
-                return cur.val
-            cur = cur.right
+            count +=1 
 
+            if count == k:
+                return cur.val
+            cur=cur.right
+
+        return -1
