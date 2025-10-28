217181918c2acaf48b30f53d05aef21413ee0cc1
Jason Bian <jasonzb@umich.edu>
2025-10-28 23:23:57 +0000

    [LeetCode Sync] Runtime - 143 ms (5.33%), Memory - 27.5 MB (5.03%)

--- solutions/0297-serialize-and-deserialize-binary-tree/solution.py
+++ solutions/0297-serialize-and-deserialize-binary-tree/solution.py
@@ -1,39 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-from collections import deque
-
+# Deserialization 
 class Codec:
 
+
     def serialize(self, root):
-        arr = []
-        q = deque()
-        q.append(root)
-        while q:
-            node = q.popleft()
-            arr.append(str(node.val) if node else " ")
-            if not node:
-                continue
-            q.append(node.left)
-            q.append(node.right)
-        while arr and arr[-1] == " ":
-            arr.pop()
+        """ Encodes a tree to a single string.
+        :type root: TreeNode
+        :rtype: str
+        """
+        def rserialize(root, string):
+            """ a recursive helper function for the serialize() function."""
+            # check base case
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.val) + ','
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
         
-        return ",".join(map(str, arr))
+        return rserialize(root, '')
+
 
     def deserialize(self, data):
-        data = data.split(',')
-        if data == ['']:
-            return None
-        res = [None if i == ' ' else int(i) for i in data]
-        return res
+        """Decodes your encoded data to tree.
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        def rdeserialize(l):
+            """ a recursive helper function for deserialization."""
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+                
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
 
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))                     
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
