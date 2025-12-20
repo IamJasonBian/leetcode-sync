dcf3d192fff06037fa1a664e9fea51e20a6c31d2
Jason Bian <jasonzb@umich.edu>
2025-12-20 01:15:14 +0000

    [LeetCode Sync] Runtime - 3 ms (86.79%), Memory - 18.4 MB (93.51%)

--- solutions/0207-course-schedule/solution.py
+++ solutions/0207-course-schedule/solution.py
@@ -1,40 +1,27 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
 
-        # Prepare the graph
-        adj_list = defaultdict(list)
-        indegree = {}
-        for dest, src in prerequisites:
-            adj_list[src].append(dest)
+        for p in prerequisites:
+            adj[p[1]].append(p[0])
+            indegree[p[0]] += 1
 
-            # Record each node's in-degree
-            indegree[dest] = indegree.get(dest, 0) + 1
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
 
-        # Queue for maintainig list of nodes that have 0 in-degree
-        # We want to pop anything that hits the threshold, regardless of the situation
-        zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
+        count = 0
+        while q:
+            curr = q.popleft()
+            count += 1
+            for next in adj[curr]:
+                indegree[next] -= 1
+                if indegree[next] == 0:
+                    q.append(next)
 
-        topological_sorted_order = []
-
-        # Until there are nodes in the Q
-        while zero_indegree_queue:
-
-            # Pop one node with 0 in-degree
-            vertex = zero_indegree_queue.popleft()
-            topological_sorted_order.append(vertex)
-
-            # Reduce in-degree for all the neighbors
-            if vertex in adj_list:
-                for neighbor in adj_list[vertex]:
-                    indegree[neighbor] -= 1
-
-                    # Add neighbor to Q if in-degree becomes 0
-                    if indegree[neighbor] == 0:
-                        zero_indegree_queue.append(neighbor)
-
-        return True if len(topological_sorted_order) == numCourses else False
+        return count == numCourses
