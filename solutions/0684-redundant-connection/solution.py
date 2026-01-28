3e8e1d99d7ef8ba6c7ad34187eeb4ac06c88291d
Jason Bian <jasonzb@umich.edu>
2026-01-28 00:00:48 +0000

    [LeetCode Sync] Runtime - 4 ms (28.84%), Memory - 19.8 MB (31.85%)

--- solutions/0684-redundant-connection/solution.py
+++ solutions/0684-redundant-connection/solution.py
@@ -1,27 +1,44 @@
-from typing import List
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        parent = list(range(len(edges) + 1)) # Initialize parent array
-        
-        def find(x):
-            # Find the root of x
-            if parent[x] != x:
-                parent[x] = find(parent[x]) # Path compression
-            return parent[x]
-        
-        def union(x, y):
-            # Union two nodes
-            root_x = find(x)
-            root_y = find(y)
-            if root_x != root_y:
-                parent[root_x] = root_y
-                return True
-            return False
-        
-        for edge in edges:
-            if not union(edge[0], edge[1]):
-                return edge
-        
-        return edges[-1] # If no cycle is found, the last edge is redundant
 
+        def make_set(n):
+            """Initialize union-find structures for n elements."""
+            return {
+                'parent': list(range(n)),
+                'rank': [0] * n,
+                'count': n
+            }
+
+        def find(uf, x):
+
+            if uf['parent'][x] != x:
+                uf['parent'][x] = find(uf, uf['parent'][x])
+            return uf['parent'][x]
+        
+        def union(uf, x, y):
+
+            root_x, root_y = find(uf, x), find(uf, y)
+            if root_x == root_y:
+                return False
+
+            if uf['rank'][root_x] < uf['rank'][root_y]:
+                root_x, root_y = root_y, root_x
+
+            uf['parent'][root_y] = root_x
+            if uf['rank'][root_x] == uf['rank'][root_y]:
+                uf['rank'][root_x] += 1
+
+            uf['count'] -= 1
+            return True
+
+        uf = make_set(len(edges)+1)
+
+        result = []
+        #union permanently loads into place
+        for edge in edges:
+            if not union(uf, edge[0], edge[1]):
+                result = edge
+        return result
+
+
+        print(f"Sets: {uf['count']}")
