e0c92c3cc6c0d4f4003ca3d9a831187b473e335c
Jason Bian <jasonzb@umich.edu>
2026-01-30 07:26:26 +0000

    [LeetCode Sync] Runtime - 15 ms (19.56%), Memory - 22.3 MB (61.46%)

--- solutions/0130-surrounded-regions/solution.py
+++ solutions/0130-surrounded-regions/solution.py
@@ -2,47 +2,53 @@ from collections import deque
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        
-        o = "O"
-        
-        n = len(board) 
-        m = len(board[0])
 
-        Q = deque()
+        """
         
-        for i in range(n):
-            if board[i][0] == o:
-                Q.append((i,0))
-            if board[i][m-1] == o:
-                Q.append((i, m-1))
-                
-        for j in range(m):
-            if board[0][j] == o:
-                Q.append((0,j))
-            if board[n-1][j] == o:
-                Q.append((n-1, j))
-                
-        def inBounds(i,j):
-            return (0 <= i < n) and (0 <= j < m)
-                
-        while Q:
-            i,j = Q.popleft()
-            board[i][j] = "#"
-            
-            for ii, jj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                if not inBounds(ii, jj):
-                    continue
-                if board[ii][jj] != o:
-                    continue
-                Q.append((ii,jj))
-                board[ii][jj] = '#'
-            
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == o:
+            Go Inwards
+
+        """
+
+        m, n = len(board), len(board[0])
+        # "Corrupted" cells from the boundary
+        boundary_bfs = deque()
+        visited = set()
+
+        for i in range(m):
+            for j in range(n):
+                if     board[i][j] == 'O' and (i in (0, m - 1) or j in (0, n - 1)):
+                    if (i, j) not in visited:  # Extra safety check
+                        boundary_bfs.append((i, j))
+                        visited.add((i, j))
+
+        print(boundary_bfs)
+        while boundary_bfs:
+            r, c = boundary_bfs.popleft()
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr, nc = r + dr, c + dc
+                # Traverse and find all Os
+                if (0 <= nr < m and 0 <= nc < n and board[nr][nc] == 'O' and (nr, nc) not in visited):
+                    visited.add((nr, nc))
+                    boundary_bfs.append((nr, nc))
+
+        print(boundary_bfs)
+        print(visited)
+        print(board)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O' and (i, j) not in visited:
                     board[i][j] = 'X'
-                elif board[i][j] == '#':
-                    board[i][j] = o
+        
+        print(f"Final board: {board}")
+
+
+
+
+
+
+
+
+
+        
+       
