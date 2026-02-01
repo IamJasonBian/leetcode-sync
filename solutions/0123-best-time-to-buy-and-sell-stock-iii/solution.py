74e4733ba6d41dc0610be79c3583653c2909037a
Jason Bian <jasonzb@umich.edu>
2026-02-01 04:12:55 +0000

    [LeetCode Sync] Runtime - 175 ms (65.93%), Memory - 33.9 MB (37.31%)

--- solutions/0123-best-time-to-buy-and-sell-stock-iii/solution.py
+++ solutions/0123-best-time-to-buy-and-sell-stock-iii/solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        k = 2
-        if 2*k >= len(prices): 
-            return sum(max(0, prices[i]-prices[i-1]) for i in range(1, len(prices)))
         
+        n = len(prices)
+        if n < 2:
+            return 0
+
+        # Left - Max Profit from one transaction in prices[0:i+1]
+
+        left = [0] * n
+        min_price = prices[0]
+        for i in range(1, n):
+            min_price = min(min_price, prices[i])
+            left[i] = max(left[i - 1], prices[i] - min_price)
+
+        # Left Slides and then Right --> 
+        # Right - Max Profit from one transaction in prices[i:n]
+        right = [0] * n
+        max_price = prices[n - 1]
+        for i in range(n - 2, 0, -1):
+            max_price = max(max_price, prices[i])
+            right[i] = max(right[i + 1], max_price - prices[i])
         
-        # Note that the DPs here are the best possible implementation for that specific value key at that point in time
-        
-        pnl = [0]*len(prices)
-        for _ in range(k):
-            val = 0
-            for i in range(1, len(pnl)): 
-                val = max(pnl[i], val + prices[i] - prices[i-1]) 
-                pnl[i] = max(pnl[i-1], val)
-        return pnl[-1]
+        # Combine: best split point for two transactions
+        # Temporal seperation
+        return max(left[i] + right[i] for i in range(n))
