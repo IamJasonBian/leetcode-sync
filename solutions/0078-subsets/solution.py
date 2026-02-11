6d833cc5c6b7bd941133ca883cfa4889fe9ecea6
Jason Bian <jasonzb@umich.edu>
2026-02-11 17:42:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (19.69%)

--- solutions/0078-subsets/solution.py
+++ solutions/0078-subsets/solution.py
@@ -1,19 +1,73 @@
+from typing import List
+
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+      def subsets(self, nums: List[int]) -> List[List[int]]:
 
-            res = []
-            subset = []
-            def dfs(i):
-                if i >= len(nums):
-                    res.append(subset.copy())
-                    return
-                
-                subset.append(nums[i])
-                dfs(i + 1)
+        '''
 
-                subset.pop()
-                dfs(i + 1)
+            O(1)
+
+                [1]
+                [1,2,3]
+                [2,3]
+                [3,3]
+
+            [[], [1], [2], [1,2], [1,2,3]]
+
+            backtrack(0)
+                0, 1, 2
+                 path = [[0, 1, 2]]
+                    path = [[1, 2]]
+
+                   1  2
+                    path = [[1, 2]]
+                         2
             
-            dfs(0)
-            return res
+            backtrack(1)
+                1, 2
+                path = [[1, 2, 3]]
 
+               
+            backtrack(2)
+                1, 2
+                path = [[1, 2]]
+
+            backtrack(3)
+                2
+                path = [[2]]
+
+            [[], [1], [2], [1,2]]
+
+
+        Permuating
+
+            |  |  | - add to res
+            |  |  |
+            |  |  |
+
+        The start stays steady (does not move)
+        How to fix the popping - the pop happens when you get to the end of an iteration
+
+        Iterations - 
+
+        * 0 to 2 - [1,2,3]
+        * 1 to 2 - 
+        * 2 to 2 
+
+        FiFo to guarantee symmetry 
+
+        '''
+      
+        res, path = [], []
+        n = len(nums)
+
+        def backtrack(start: int):
+            res.append(path.copy())
+
+            for i in range(start, n):
+                path.append(nums[i])
+                backtrack(i + 1)
+                path.pop()
+
+        backtrack(0)
+        return res
